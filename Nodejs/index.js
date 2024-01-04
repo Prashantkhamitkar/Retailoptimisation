8979bf121658ed17092d69f0c925942cdadd9b2e
prashant <prashantkhamitkar@gmail.com>
2024-01-04 23:11:09 +0530

    added

---
+++ Nodejs/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ExcelJS = require('exceljs');
+const cors=require('cors')
+const app = express();
+app.use(cors({
+  origin: '*',
+  methods: '*',
+  allowedHeaders: '*',
+}));
+const port = 3001;
+
+app.get('/read-excel', (req, res) => {
+  const filePath = 'Chartdata.xlsx';
+
+  const workbook = new ExcelJS.Workbook();
+
+  workbook.xlsx
+    .readFile(filePath)
+    .then(() => {
+      const worksheet = workbook.getWorksheet(1); // Get the first worksheet
+      const data = [];
+
+      worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+        // Access each row and its cell values
+        data.push({ rowNumber, values: row.values });
+      });
+
+      res.json({ excelData: data });
+    })
+    .catch(error => {
+      console.error('Error reading the file:', error);
+      res.status(500).json({ error: 'Error reading the Excel file' });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
